566fa27d1ec8efad8c1e3a0c3c70a4efe075614c
Titan Chen <titanchen@yahoo.com>
2024-09-07 13:11:35 -0400

    changes to amplify

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={reactLogo} className="logo react" alt="React logo" />
+
+        <h1>Hello from Amplify</h1>
       </header>
     </div>
   );
